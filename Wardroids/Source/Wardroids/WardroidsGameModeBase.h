1cf4946914aa498af32955a8799b6c1ae9db4d8d
Chriatian Edge <christian.m.edge@gmail.com>
2020-05-31 11:03:36 -0400

    init of game files

---
+++ Wardroids/Source/Wardroids/WardroidsGameModeBase.h
@@ -0,0 +1,17 @@
+// Copyright 1998-2019 Epic Games, Inc. All Rights Reserved.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/GameModeBase.h"
+#include "WardroidsGameModeBase.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class WARDROIDS_API AWardroidsGameModeBase : public AGameModeBase
+{
+	GENERATED_BODY()
+	
+};
